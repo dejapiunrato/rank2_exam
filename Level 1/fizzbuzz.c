d998c1066d17bae7a90b7a918c78867c4d82e2f2
Pi Sevilla Hernandez <psevilla@c1r17s6.42madrid.com>
2024-11-28 16:49:49 +0100

    -

---
+++ Level 1/fizzbuzz.c
@@ -0,0 +1,77 @@
+/* Assignment name  : fizzbuzz
+Expected files   : fizzbuzz.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that prints the numbers from 1 to 100, each separated by a
+newline.
+
+If the number is a multiple of 3, it prints 'fizz' instead.
+
+If the number is a multiple of 5, it prints 'buzz' instead.
+
+If the number is both a multiple of 3 and a multiple of 5, it prints 'fizzbuzz' instead.
+
+Example:
+
+$>./fizzbuzz
+1
+2
+fizz
+4
+buzz
+fizz
+7
+8
+fizz
+buzz
+11
+fizz
+13
+14
+fizzbuzz
+[...]
+97
+98
+fizz
+buzz
+$> */
+
+#include <unistd.h>
+
+void	printnum(int i)
+{
+	char	c;
+
+	if (i >= 10)
+		printnum(i / 10);
+	c = (i % 10) + '0';
+	write(1, &c, 1);
+}
+
+void	printstr(char *s)
+{
+	while (*s)
+	{
+		write(1, s++, 1);
+	}
+}
+
+int	main(void)
+{
+	int	i;
+
+	i = 1;
+	while (i <= 100)
+	{
+		if (i % 3 == 0)
+			printstr("fizz");
+		if (i % 5 == 0)
+			printstr("buzz");
+		if (i % 3 != 0 && i % 5 != 0)
+			printnum(i);
+		i++;
+		write(1, "\n", 1);
+	}
+	return (0);
+}
---
+++ Level 1/repeat_alpha.c
@@ -0,0 +1,62 @@
+/* Assignment name  : repeat_alpha
+Expected files   : repeat_alpha.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program called repeat_alpha that takes a string and display it
+repeating each alphabetical character as many times as its alphabetical index,
+followed by a newline.
+
+'a' becomes 'a', 'b' becomes 'bb', 'e' becomes 'eeeee', etc...
+
+Case remains unchanged.
+
+If the number of arguments is not 1, just display a newline.
+
+Examples:
+
+$>./repeat_alpha "abc"
+abbccc
+$>./repeat_alpha "Alex." | cat -e
+Alllllllllllleeeeexxxxxxxxxxxxxxxxxxxxxxxx.$
+$>./repeat_alpha 'abacadaba 42!' | cat -e
+abbacccaddddabba 42!$
+$>./repeat_alpha | cat -e
+$
+$>
+$>./repeat_alpha "" | cat -e
+$
+$> */
+
+#include <unistd.h>
+
+void	ft_putchr(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (argc == 2)
+	{
+		while (argv[1][i])
+		{
+			if (argv[1][i] >= 'a' && argv[1][i] <= 'z')
+			{
+				j = argv[1][i] - 'a' + 1;
+				while (j--)
+					ft_putchr(argv[1][i]);				
+			}
+			else
+				ft_putchr(argv[1][i]);
+			i++;
+		}
+	}
+	ft_putchr('\n');
+	return (0);
+}
