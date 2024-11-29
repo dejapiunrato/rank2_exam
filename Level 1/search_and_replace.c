08e843f2f5dd09a05786b063a9bc32a2ac0db588
Piesito <83318869+dejapiunrato@users.noreply.github.com>
2024-11-29 11:59:27 +0100

    Update search_and_replace.c

--- Level 1/search_and_replace.c
+++ Level 1/search_and_replace.c
@@ -30,6 +30,16 @@ eNcOre Un ExEmPle Pas Facile a Ecrire $
 
 #include <unistd.h>
 
+int    ft_strlen(char *str)
+{
+    int    i;
+
+    i = 0;
+    while (str[i])
+        i++;
+    return (i);
+}
+
 int main(int argc, char **argv)
 {
     int i;
