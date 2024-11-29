5c8bfca8bd56e075014b742a858d16e504b58641
Piesito <83318869+dejapiunrato@users.noreply.github.com>
2024-11-29 12:09:05 +0100

    Update search_and_replace.c

--- Level 1/search_and_replace.c
+++ Level 1/search_and_replace.c
@@ -45,12 +45,12 @@ int main(int argc, char **argv)
     int i;
     
     i = 0;
-    if (argc == 4 && ft_strlen(argv[2] == 1) && ft_strlen(argv[3] == 1))
+    if (argc == 4 && ft_strlen(argv[2]) == 1 && ft_strlen(argv[3]) == 1)
     {
         while (argv[1][i])
         {
-            if (argv[1][i] == argv[2][1])
-                argv[1][i] = argv[3][1];
+            if (argv[1][i] == argv[2][0])
+                argv[1][i] = argv[3][0];
             write(1, &argv[1][i], 1);
             i++;
         }
