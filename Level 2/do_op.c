aeb698dc8c9ee6595fb1ebbba1385bb5840e4557
Piesito <83318869+dejapiunrato@users.noreply.github.com>
2024-11-29 15:12:49 +0100

    Update do_op.c

--- Level 2/do_op.c
+++ Level 2/do_op.c
@@ -33,7 +33,7 @@ $
 #include <stdlib.h>
 #include <stdio.h>
 
-void do_op(char *s1, char op, char *s2)
+int do_op(char *s1, char op, char *s2)
 {
   int  num1;
   int  num2;
@@ -58,7 +58,7 @@ void do_op(char *s1, char op, char *s2)
 int  main(int argc, char **argv)
 {
   if (argc == 4)
-    do_op(argv[1], argv[2][0], argv[3]);
+    printf("%d", do_op(argv[1], argv[2][0], argv[3]);
   write(1, "\n", 1);
   return (0);
 }
