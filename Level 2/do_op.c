82302dc8dd676c22cb229e51d532deef64e1d7a9
Piesito <83318869+dejapiunrato@users.noreply.github.com>
2024-11-29 15:15:38 +0100

    Update do_op.c

--- Level 2/do_op.c
+++ Level 2/do_op.c
@@ -58,7 +58,7 @@ int do_op(char *s1, char op, char *s2)
 int  main(int argc, char **argv)
 {
   if (argc == 4)
-    printf("%d", do_op(argv[1], argv[2][0], argv[3]);
+    printf("%d", do_op(argv[1], argv[2][0], argv[3]));
   write(1, "\n", 1);
   return (0);
 }
