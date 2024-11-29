e4272ed92e2ed38a245e330544a4ca5a5cdc22d0
Piesito <83318869+dejapiunrato@users.noreply.github.com>
2024-11-29 14:11:53 +0100

    Update snake_to_camel.c

--- Level 2/snake_to_camel.c
+++ Level 2/snake_to_camel.c
@@ -33,7 +33,7 @@ int  main(int argc, char **argv)
   {
     while (argv[1][i])
     {
-      if (argv[1][i] >= 'A' || argv[1][i] <= 'Z')
+      if (argv[1][i] >= 'A' && argv[1][i] <= 'Z')
       {
         write(1, "_", 1);
         argv[1][i] = argv[1][i] - ('a' - 'A');
